8e89ddae5f911a9c91de939c0cf66d4a49760313
MaoCarmona <89806416+MaoCarmona@users.noreply.github.com>
2024-04-08 01:59:43 -0500

    Store_V1

---
+++ src/app/products.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 100,
+    image: "https://pooltableoffers.com.au/wp-content/uploads/2023/10/product-test.webp"
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 200,
+    image: "https://pooltableoffers.com.au/wp-content/uploads/2023/10/product-test.webp",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 300,
+    image: "https://pooltableoffers.com.au/wp-content/uploads/2023/10/product-test.webp",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: 400,
+    image: "https://pooltableoffers.com.au/wp-content/uploads/2023/10/product-test.webp",
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    price: 500,
+    image: "https://pooltableoffers.com.au/wp-content/uploads/2023/10/product-test.webp",
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    price: 600,
+    image: "https://pooltableoffers.com.au/wp-content/uploads/2023/10/product-test.webp",
+  },
+  {
+    id: 7,
+    name: "Product 7",
+    price: 700,
+    image: "https://pooltableoffers.com.au/wp-content/uploads/2023/10/product-test.webp",
+  },
+];
